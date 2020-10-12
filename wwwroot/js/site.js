aa3d4c87e9dfec8bf26c6242327d177ffa7d47f6
Daniel Polo <daniel_polo@live.com>
2020-10-12 19:59:17 -0300

    Ajustes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,24 +1,55 @@
-﻿function BotaoDadosGeraisProximo()
-{
+﻿var DivDadosGerais = "#DadosGerais";
+var DivVisaoGeral = "#VisaoGeral";
+var DivDetalhes = "#Detalhes";
+var DivAnaliseAjuste = "#AnaliseAjuste";
 
+function OcultarDadosIniciais()
+{
+    $(DivVisaoGeral).hide(300);
+    $(DivDetalhes).hide(300);
+    $(DivAnaliseAjuste).hide(300);
 }
 
-function BotaoVisaoGeralAnterior() {
+function BotaoDadosGeraisProximo()
+{
+    $(DivDadosGerais).hide(300);
+    $(DivVisaoGeral).show(300);
+    $(DivDetalhes).hide(300);
+    $(DivAnaliseAjuste).hide(300);
+}
 
+function BotaoVisaoGeralAnterior()
+{
+    $(DivDadosGerais).show(300);
+    $(DivVisaoGeral).hide(300);
+    $(DivDetalhes).hide(300);
+    $(DivAnaliseAjuste).hide(300);
 }
 
 function BotaoVisaoGeralProximo() {
-
+    $(DivDadosGerais).hide(300);
+    $(DivVisaoGeral).hide(300);
+    $(DivDetalhes).show(300);
+    $(DivAnaliseAjuste).hide(300);
 }
 
 function BotaoDetalhesAnterior() {
-
+    $(DivDadosGerais).hide(300);
+    $(DivVisaoGeral).show(300);
+    $(DivDetalhes).hide(300);
+    $(DivAnaliseAjuste).hide(300);
 }
 
 function BotaoDetalhesProximo() {
-
+    $(DivDadosGerais).hide(300);
+    $(DivVisaoGeral).hide(300);
+    $(DivDetalhes).hide(300);
+    $(DivAnaliseAjuste).show(300);
 }
 
 function BotaoAnaliseAjusteAnterior() {
-
+    $(DivDadosGerais).hide(300);
+    $(DivVisaoGeral).hide(300);
+    $(DivDetalhes).show(300);
+    $(DivAnaliseAjuste).hide(300);
 }
